ddaf8b8fb07821c84101a26cfba4e79fea19c0ae
瑞雯 <53329456+ArvinRoad@users.noreply.github.com>
2023-11-24 15:07:12 +0800

    Update FPSPlayerController.cpp

--- Source/Scorpio/Private/FPSPlayerController.cpp
+++ Source/Scorpio/Private/FPSPlayerController.cpp
@@ -1,5 +1,11 @@
 #include "FPSPlayerController.h"
+#include "GameFramework/PlayerController.h"
 
 void AFPSPlayerController::PlayerCameraShake(TSubclassOf<UCameraShakeBase> CameraShake) {
-	ClientPlayCameraShake(CameraShake,1,ECameraShakePlaySpace::CameraLocal,FRotator::ZeroRotator);
+
+	/* 5.3 中 ClientPlayCameraShake 更名为 ClientStartCameraShake
+	 *  ClientPlayCameraShake(CameraShake, 1, ECameraShakePlaySpace::CameraLocal, FRotator::ZeroRotator);
+	 */
+
+	ClientStartCameraShake(CameraShake, 1, ECameraShakePlaySpace::CameraLocal, FRotator::ZeroRotator);
 }
